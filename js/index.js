9464912b3e43be233de4b9a24d91e4da4834f1e3
EdsonCampanhao <edsonlemos0525@gmail.com>
2022-12-07 07:36:15 -0300

    att

--- js/index.js
+++ js/index.js
@@ -13,16 +13,16 @@ async function findPokemon(pokemon) {
 function createStructure(){
     if(pokemonTeam.children.length<4){
     pokemon = pokemonTeam.appendChild(document.createElement('div'));
-    pokemon.classList.add('.createTeamContainer__pokemon')
+    pokemon.classList.add('createTeamContainer__pokemon')
 
     pokemonImg=pokemon.appendChild(document.createElement('img'))
-    pokemonImg.classList.add('.createTeamContainer__img')
+    pokemonImg.classList.add('createTeamContainer__img')
 
     pokemonNm=pokemon.appendChild(document.createElement('p'))
-    pokemonNm.classList.add('.createTeamContainer__name')
+    pokemonNm.classList.add('createTeamContainer__name')
 
     pokemonTp=pokemon.appendChild(document.createElement('div'))
-    pokemonTp.classList.add('.createTeamContainer__type')}
+    pokemonTp.classList.add('createTeamContainer__type')}
 }
 function addData(img,name,type1,type2){
    let lastPokemon= pokemonTeam.children[pokemonTeam.children.length-1]
@@ -36,7 +36,7 @@ function addData(img,name,type1,type2){
 button.addEventListener('click', async (event)=>{
     event.preventDefault()
     let fetchedPokemon = await findPokemon(PokemonName.value)
-    createStructure()
+    if(PokemonName.value!=''){createStructure()
     console.log(fetchedPokemon)
     if(pokemonTeam.children.length<=4){
     if(fetchedPokemon.types.length<2){
@@ -44,7 +44,7 @@ button.addEventListener('click', async (event)=>{
     }
     else{
         addData(fetchedPokemon.sprites.other.dream_world.front_default,fetchedPokemon.name,fetchedPokemon.types[0].type.name,fetchedPokemon.types[1].type.name)  
-    }}
+    }}}
 }
 )
 
